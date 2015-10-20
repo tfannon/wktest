113c1f7d574cc6ad6dd1d47e143365469211c7ec
arothberg <Adam@adams-macbook-pro.local>
2015-10-19 20:09:40 -0400

    Forms

--- ParsePush/ProcedureFormControllerViewController.swift
+++ ParsePush/ProcedureFormControllerViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ProcedureFormControllerViewController: UIViewController {
+class ProcedureFormControllerViewController: UIViewController  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		
         // Do any additional setup after loading the view.
     }
 
